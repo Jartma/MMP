6660539435be5e9406f1e7059780e517e453df99
Chiara Welz <c.welz@campus.lmu.de>
2021-07-13 07:52:45 +0200

    add Backgroundmusic for the Scrrens; add Sounds for letter & heart; destroy collision gameObjects

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -60,24 +60,58 @@ public class Health : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        //check if edgeCollder2d was hit
+        //check if edgeCollider2d was hit
         if(edgeCol.IsTouching(collision.collider)){
             if(collision.gameObject.tag=="Letter" && lastColName != collision.collider.name)
                 {
+                    //letter disabled
+                    collision.gameObject.GetComponent<Renderer>().enabled = false;
+                    
+                    //Sound for Collision with letter 
+                    AudioSource audio = collision.gameObject.GetComponent<AudioSource>();
+                    audio.Play();
+
                     //only take damage if lastColName is not the same as last collision,
                     //otherwise takedamage will repeat as long as letterbox is touching the edgeCollider2d
                     TakeDamage(1);
                     lastColName = collision.collider.name;
+                    
+                    //destroy letter
+                    Destroy(collision.gameObject, audio.clip.length);
                 }
 
             if (collision.gameObject.tag == "Heart" && currentHealth == startingHealth)
             {
+                //heart disabled
+                collision.gameObject.GetComponent<Renderer>().enabled = false;
+                
+                //Sound for Energy
+                AudioSource audio = collision.gameObject.GetComponent<AudioSource>();
+                audio.Play();
+                
+                //get 10points for score
                 ScoreScript.scoreValue += 10;
+                
+                //destroy heart
+                Destroy(collision.gameObject, audio.clip.length);
+                
+                
             } 
             
             if (collision.gameObject.tag == "Heart")
             {
+                //letter disabled
+                collision.gameObject.GetComponent<Renderer>().enabled = false;
+                
+                //Sound for Energy
+                AudioSource audio = collision.gameObject.GetComponent<AudioSource>();
+                audio.Play();
+                
+                //get 1 energy
                 AddHealth(1);
+                
+                //destroy heart
+                Destroy(collision.gameObject, audio.clip.length);
             }
         }
     }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -52,6 +52,7 @@ public class PlayerMovement : MonoBehaviour
         {
             grounded = true;
         }
+        
     }
 
 }
