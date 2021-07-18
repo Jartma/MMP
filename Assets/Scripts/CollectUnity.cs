943cd837e83e2270fa1f343180e39f70e1ea2206
Sebastian Hörberg <s.hoerberg@campus.lmu.de>
2021-07-18 13:37:42 +0200

    meeting anpassungen

--- Assets/Scripts/CollectUnity.cs
+++ Assets/Scripts/CollectUnity.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class CollectUnity : MonoBehaviour
 {
@@ -75,6 +76,9 @@ public class CollectUnity : MonoBehaviour
                 soundCoin.Play();
                 //Element destroyed
                 Destroy(collision.gameObject);
+        }
+        if (U.enabled && N.enabled && I.enabled && T.enabled && Y.enabled) {
+            SceneManager.LoadScene("Level 3");
         }
         
     }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -59,7 +59,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Ground")
+        if(collision.gameObject.tag == "Ground" || collision.gameObject.tag == "Letter")
         {
             grounded = true;
         }
